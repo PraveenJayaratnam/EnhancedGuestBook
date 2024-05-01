b914d6dd3d5c25f10f2d85565ee9c26d68f546a1
Praveen Jayaratnam <praveen18.warner@gmail.com>
2024-05-02 00:52:16 +0530

    Add project files.

---
+++ GuestBook/Program.cs
@@ -0,0 +1,44 @@
+﻿using GuestLibrary.Models;
+
+public class GuestBook
+{
+    public static List<Guest> guests = new List<Guest>();
+
+    public static void Main(string[] args)
+    {
+        GetGuestInfo();
+        Console.Clear();
+        DisplayUserInfo();
+        Console.ReadLine(); 
+    }
+
+    private static void DisplayUserInfo()
+    {
+
+        foreach (Guest guest in guests)
+        {
+            Console.WriteLine(guest.UserInfo);
+        }
+    }
+    private static void GetGuestInfo()
+    {
+        string moreGuestToAdd = "";
+        do
+        {
+            Guest guest = new Guest();
+            guest.FirstName = GetUserResponse("Enter your FirstName: ");
+            guest.LastName = GetUserResponse("Enter your LastName: ");
+            guest.MessageToHost = GetUserResponse("Enter the message to host: ");
+            guests.Add(guest);
+            moreGuestToAdd = GetUserResponse("Do you have more guest to add: ");
+        } while (moreGuestToAdd.ToLower() == "yes");
+    }
+
+    private static string GetUserResponse(string question)
+    {
+        Console.Write(question);
+        string output = Console.ReadLine();
+        return output;
+    }
+
+}
---
+++ GuestLibrary/Models/Guest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestLibrary.Models
+{
+    public class Guest
+    {
+        public string FirstName { get; set; } = string.Empty; 
+        public string LastName { get; set; } = string.Empty;
+        public string MessageToHost { get; set; } = string.Empty;
+        public string UserInfo
+        {
+            get
+            {
+                return $"{FirstName} {LastName} : {MessageToHost}";
+            }
+        }
+    }
+}
